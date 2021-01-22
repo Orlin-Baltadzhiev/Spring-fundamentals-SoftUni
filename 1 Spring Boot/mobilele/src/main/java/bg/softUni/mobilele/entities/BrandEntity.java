6ef09e7b8d5fd5a8a183fa69a39ee114bf19ac9c
OpKata <60965798+OpKata@users.noreply.github.com>
2021-01-22 15:33:46 +0200

    Adding brands and models.

--- 1 Spring Boot/mobilele/src/main/java/bg/softUni/mobilele/entities/BrandEntity.java
+++ 1 Spring Boot/mobilele/src/main/java/bg/softUni/mobilele/entities/BrandEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "brands")
 public class BrandEntity extends BaseEntity {
 
 
---
+++ 1 Spring Boot/mobilele/src/main/java/bg/softUni/mobilele/entities/enums/ModelCategoryEnum.java
@@ -0,0 +1,8 @@
+package bg.softUni.mobilele.entities.enums;
+
+public enum ModelCategoryEnum {
+    CAR,
+    BUS,
+    TRUCK,
+    MOTORCYCLE;
+}
