80f36f59edff291b6076e778ce038e5987a3264d
OpKata <60965798+OpKata@users.noreply.github.com>
2021-02-01 16:40:50 +0200

    cookie example

---
+++ Cookie  Example/src/test/java/com/coockies/CoockiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coockies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoockiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
